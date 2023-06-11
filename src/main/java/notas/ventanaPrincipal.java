2e8a68e74a3e7d2bab9725b7052dd2c9c2b1b613
GLOOMYY <sebas.mesa.montoya@gmail.com>
2023-06-10 19:39:08 -0500

    El listener

--- src/main/java/notas/ventanaPrincipal.java
+++ src/main/java/notas/ventanaPrincipal.java
@@ -1,177 +1,74 @@
 package notas;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-
-public class ventanaPrincipal extends javax.swing.JFrame {
+public class ventanaPrincipal extends javax.swing.JFrame implements ActionListener {
 
     public ventanaPrincipal() {
         initComponents();
     }
-    
-    ActionListener {
-        // Un contenedor de elementos gráficos
-        private Container contenedor;
-        // Etiquetas estáticas de cada nota
-        private JLabel nota1, nota2, nota3, nota4, nota5, promedio, desviación, mayor, menor;
-        // Campos de ingreso de cada nota
-        private JTextField campoNota1, campoNota2, campoNota3, campoNota4, campoNota5;
-        // Botones para realizar cálculos y para borrar las notas
-        private JButton calcular, limpiar;
- 
+
+    private Container contenedor;
+    private JLabel nota1, nota2, nota3, nota4, nota5, promedio, desviacion, mayor, menor;
+    private JTextField campoNota1, campoNota2, campoNota3, campoNota4, campoNota5;
+    private JButton calcular, limpiar, salir;
+
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        campoNota1 = new javax.swing.JTextField();
-        campoNota2 = new javax.swing.JTextField();
-        campoNota3 = new javax.swing.JTextField();
-        campoNota4 = new javax.swing.JTextField();
-        campoNota5 = new javax.swing.JTextField();
-        calcular = new javax.swing.JButton();
-        desviacion = new javax.swing.JLabel();
-        mayor = new javax.swing.JLabel();
-        menor = new javax.swing.JLabel();
-        promedio = new javax.swing.JLabel();
-        limpiar = new javax.swing.JButton();
-        salir = new javax.swing.JButton();
+        contenedor = getContentPane();
+        nota1 = new JLabel("Nota 1");
+        nota2 = new JLabel("Nota 2");
+        nota3 = new JLabel("Nota 3");
+        nota4 = new JLabel("Nota 4");
+        nota5 = new JLabel("Nota 5");
+        campoNota1 = new JTextField();
+        campoNota2 = new JTextField();
+        campoNota3 = new JTextField();
+        campoNota4 = new JTextField();
+        campoNota5 = new JTextField();
+        calcular = new JButton("Calcular");
+        limpiar = new JButton("Limpiar");
+        salir = new JButton("Salir");
+        promedio = new JLabel("Promedio = ");
+        desviacion = new JLabel("Desviación estándar = ");
+        mayor = new JLabel("Valor mayor = ");
+        menor = new JLabel("Valor menor = ");
+        calcular.addActionListener(this);
+        limpiar.addActionListener(this);
+        salir.addActionListener(this);
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("Nota 1");
-
-        jLabel2.setText("Nota 2");
-
-        jLabel3.setText("Nota 3");
-
-        jLabel4.setText("Nota 4");
-
-        jLabel5.setText("Nota 5");
-
-        campoNota1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                campoNota1ActionPerformed(evt);
-            }
-        });
-
-        calcular.setText("Calcular");
-
-        desviacion.setText("Desviación estándar = ");
-
-        mayor.setText("Valor mayor = ");
-
-        menor.setText("Valor menor = ");
-
-        promedio.setText("Promedio = ");
-
-        limpiar.setText("Limpiar");
-
-        salir.setText("Salir");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(campoNota4, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(campoNota3, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(campoNota2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(campoNota1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(desviacion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(mayor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(menor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(promedio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(campoNota5, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(74, 74, 74)
-                        .addComponent(calcular)
-                        .addGap(18, 18, 18)
-                        .addComponent(limpiar)
-                        .addGap(18, 18, 18)
-                        .addComponent(salir)))
-                .addContainerGap(40, Short.MAX_VALUE))
-        );
-
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jLabel1, jLabel2, jLabel3, jLabel4, jLabel5});
-
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)
-                            .addComponent(campoNota1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(campoNota2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel3)
-                            .addComponent(campoNota3, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel4)
-                            .addComponent(campoNota4, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(promedio, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(desviacion, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(mayor, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(menor, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(campoNota5, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(calcular)
-                    .addComponent(limpiar)
-                    .addComponent(salir))
-                .addGap(30, 30, 30))
-        );
-
-        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jLabel1, jLabel2, jLabel3, jLabel4, jLabel5});
-
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.add(nota1);
+        panel.add(nota2);
+        panel.add(nota3);
+        panel.add(nota4);
+        panel.add(nota5);
+        panel.add(campoNota1);
+        panel.add(campoNota2);
+        panel.add(campoNota3);
+        panel.add(campoNota4);
+        panel.add(campoNota5);
+        panel.add(calcular);
+        panel.add(limpiar);
+        panel.add(salir);
+        panel.add(promedio);
+        panel.add(desviacion);
+        panel.add(mayor);
+        panel.add(menor);
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void campoNota1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campoNota1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_campoNota1ActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -180,7 +77,7 @@ public class ventanaPrincipal extends javax.swing.JFrame {
             }
         });
     }
-    
+
     /**
     * Método que gestiona los eventos generados en la ventana principal
     */
@@ -189,14 +86,14 @@ public class ventanaPrincipal extends javax.swing.JFrame {
         // @param evento el objeto ActionEvent que representa la acción del usuario
         if (evento.getSource() == calcular) { /* Si se pulsa el botón*/
             calculos notas = new calculos(); // Se crea un objeto Notas
-            
+
             if (campoNota1.getText().isEmpty() || campoNota2.getText().isEmpty() ||
                 campoNota3.getText().isEmpty() || campoNota4.getText().isEmpty() ||
                 campoNota5.getText().isEmpty()) {
             JOptionPane.showMessageDialog(this, "Debe ingresar todas las notas.", "Error", JOptionPane.ERROR_MESSAGE);
             return;
             }
-            
+
             try {
                 notas.listaNotas[0] = Double.valueOf(campoNota1.getText());
                 notas.listaNotas[1] = Double.valueOf(campoNota2.getText());
@@ -229,7 +126,7 @@ public class ventanaPrincipal extends javax.swing.JFrame {
                 campoNota4.setText("");
                 campoNota5.setText("");
         }
-        
+
         if (evento.getSource()==salir) {
             dispose();
         }
