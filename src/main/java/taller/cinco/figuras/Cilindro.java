48b3c3b8d7451cd7295238994511d1028eac0972
CarlosSanz1505 <csanchezor@unal.edu.co>
2023-06-14 19:25:00 -0500

    Clases e Interfaces Cilindro y Pirámide

--- src/main/java/taller/cinco/figuras/Cilindro.java
+++ src/main/java/taller/cinco/figuras/Cilindro.java
@@ -1,4 +1,13 @@
 package taller.cinco.figuras;
 
 public class Cilindro extends FiguraGeometrica {
+    public static double calcularVolumen(double radio, double altura) {
+        double volumen = Math.PI * Math.pow(radio, 2) * altura;
+        return 0.01*Math.round(100*volumen);
+    }
+
+    public static double calcularSuperficie(double radio, double altura) {
+        double superficie = 2 * Math.PI * Math.pow(radio, 2) + 2 * Math.PI*radio * altura;
+        return 0.01*Math.round(100*superficie);
+    }
 }
--- src/main/java/taller/cinco/figuras/Piramide.java
+++ src/main/java/taller/cinco/figuras/Piramide.java
@@ -1,4 +1,13 @@
 package taller.cinco.figuras;
 
 public class Piramide extends FiguraGeometrica {
+    public static double calcularVolumen(double base, double altura, double apotema) {
+        double volumen = Math.pow(base, 2) * altura / 3;
+        return 0.01*Math.round(100*volumen);
+    }
+
+    public static double calcularSuperficie(double base, double altura, double apotema) {
+        double superficie = Math.pow(base, 2) + 2*(base*apotema);
+        return 0.01*Math.round(100*superficie);
+    }
 }
